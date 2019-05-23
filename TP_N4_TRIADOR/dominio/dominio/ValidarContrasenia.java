9dbc20732c4cbdcf9cf782706a20552c59b50cdf
Daniela Triador <daniitriador@gmail.com>
2019-05-23 10:45:50 -0300

    TP 4 - Se modifico la clase Validaciones y se cambio por ValidarContrasenia

---
+++ TP_N4_TRIADOR/dominio/dominio/ValidarContrasenia.java
@@ -0,0 +1,15 @@
+package dominio;
+
+public class ValidarContrasenia {
+
+	public static boolean esLargoValido(String passNoValida01) {
+		if (passNoValida01 == null)
+			return false;
+		
+		if (passNoValida01.length() < 10)
+			return false;
+		else
+			return true;
+	}
+
+}
